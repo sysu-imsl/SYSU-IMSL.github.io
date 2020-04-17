3ba25acfe9e1180114b466db3969ed3319f3d59a
许琦 <splendid@xuqideMacBook-Pro.local>
2020-04-17 16:26:39 +0800

    fix a bug in sketchcoco

--- assets/js/getImages.js
+++ assets/js/getImages.js
@@ -9,74 +9,40 @@ function onClick(arg) {
     if (data_ == "Objects") {
         sketch_path = 'data/Object/Sketch/';
         gt_path = 'data/Object/GT/';
-        json_path = 'https://github.com/sysu-imsl/SketchyCOCO/blob/master/data/Object.json'
+        json_path = '/Object.json'
     } else {
         sketch_path = 'data/Scene/Sketch/';
         gt_path = 'data/Scene/GT/';
-        json_path = 'https://github.com/sysu-imsl/SketchyCOCO/blob/master/data/Scene.json'
+        json_path = '/Scene.json'
     }
-    $.getJSON(json_path, function (data){
-        console.log(data_type)
-        if (data_ == "Objects") {
-            var tmp = data[data_type.toString()]
-        } else {
-            var tmp = data.data;
-        }
-        console.log(tmp)
-        var counter = 0;
-        for (var n = 0; n<tmp.length; n++) {
-            path = tmp[n];
-            if (counter % 2 == 0 && counter != 0) {
-                result_array.push("        </tr>\n        <tr>\n          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
-                result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
+    $.ajax({
+        type:'get',
+        url:json_path,
+        dataType:'json',
+        success:function(data){
+            console.log(data_type)
+            if (data_ == "Objects") {
+                var tmp = data[data_type.toString()]
             } else {
-                result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
-                result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
+                var tmp = data.data;
             }
-            counter++;
+            console.log(tmp)
+            var counter = 0;
+            for (var n = 0; n<tmp.length; n++) {
+                path = tmp[n];
+                if (counter % 2 == 0 && counter != 0) {
+                    result_array.push("        </tr>\n        <tr>\n          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
+                    result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
+                } else {
+                    result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
+                    result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
+                }
+                counter++;
+            }
+
+            output.innerHTML = "              <table>\n        <tr>\n" + result_array.join("\n") + "        </tr>\n      </table>";
         }
-
-        output.innerHTML = "              <table>\n        <tr>\n" + result_array.join("\n") + "        </tr>\n      </table>";
-
-
-    });
-    // $.getJSON("http://query.yahooapis.com/v1/public/yql", {
-
-    //     q: 'select * from json where url="https://github.com/sysu-imsl/SketchyCOCO/blob/master/data/Scene.json"',
-
-    //     format: "json"
-
-    // }, function(data) {
-    //     console.log(data.query.results.json)
-    // });
-    // $.ajax({
-    //     type:'get',
-    //     url:json_path,
-    //     dataType:'json',
-    //     success:function(data){
-    //         console.log(data_type)
-    //         if (data_ == "Objects") {
-    //             var tmp = data[data_type.toString()]
-    //         } else {
-    //             var tmp = data.data;
-    //         }
-    //         console.log(tmp)
-    //         var counter = 0;
-    //         for (var n = 0; n<tmp.length; n++) {
-    //             path = tmp[n];
-    //             if (counter % 2 == 0 && counter != 0) {
-    //                 result_array.push("        </tr>\n        <tr>\n          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
-    //                 result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
-    //             } else {
-    //                 result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + sketch_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
-    //                 result_array.push("          <td>\n            <a href=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path + ">" + "<img src=" + "https://github.com/sysu-imsl/SketchyCOCO/blob/master/" + gt_path + path +  " alt=\"\" /></div></a>" + "\n       </td>");
-    //             }
-    //             counter++;
-    //         }
-
-    //         output.innerHTML = "              <table>\n        <tr>\n" + result_array.join("\n") + "        </tr>\n      </table>";
-    //     }
-    // })
+    })
 
 
 }
